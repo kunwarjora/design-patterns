bfee7b8e77b9c34a4e271201447f46df738f4fc6
Kunwar Jora <kunwarjora@gmail.com>
2026-02-05 15:47:52 +0530

    FactoryMethod design pattern is addedd.

---
+++ creational/FactoryMethod.java
@@ -0,0 +1,60 @@
+package creational;
+
+//Product Interface:
+interface Pizza{
+    void prepare();
+}
+
+//ConcreteProduct implementation
+class MargaritaPizza implements Pizza{
+    MargaritaPizza(){
+        prepare();
+    }
+
+    @Override
+    public void prepare() {
+        System.out.println("Preparing Margarita");
+    }
+}
+
+class PepperoniPizza implements Pizza{
+    PepperoniPizza(){
+        prepare();
+    }
+    @Override
+    public void prepare() {
+        System.out.println("Preparing Pepperoni Pizza");
+    }
+}
+
+// Creator Interface
+interface PizzaFactory{
+    Pizza createPizza();
+}
+// ConcreteCreator class
+class MargheritaPizzaFactory implements PizzaFactory{
+
+    @Override
+    public Pizza createPizza() {
+        return new MargaritaPizza();
+    }
+}
+class PepperoniPizzaFactory implements PizzaFactory{
+
+    @Override
+    public Pizza createPizza() {
+        return new PepperoniPizza();
+    }
+}
+// Client Code
+public class FactoryMethod {
+    public static void main(String[] args) {
+//      The client code asks the user for a choice, picks the corresponding Creator, and then uses that creator for the rest of the application's lifecycle
+//      The choice is made using if statements or switch cases.
+
+        PizzaFactory pizzaFactory = new MargheritaPizzaFactory();
+        Pizza pizza = pizzaFactory.createPizza();
+        pizzaFactory = new PepperoniPizzaFactory();
+        pizza= pizzaFactory.createPizza();
+    }
+}
